efe103964d007f7bf18e7a179f06cd3339d61800
Tiago Moreira <tiagomoreira@nqda.pt>
2020-06-03 12:30:19 +0100

    Add styles components to antd component

--- nextjs-antdesign/pages/index.js
+++ nextjs-antdesign/pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { Button } from 'antd';
+import styled from 'styled-components';
+
+const ButtonPrimary = styled(Button)`
+  border-radius: 20px;
+  height: 50px;
+  
+  &.green {
+    border-color: green;
+    background-color: green;
+
+    &:hover {
+      border-color: red;
+      background-color: red;
+    }
+  }
+`
 
 export default function Home() {
   return (
@@ -10,7 +26,18 @@ export default function Home() {
       </Head>
 
       <section>
-        <Button type="primary">Primary Button</Button>
+        <ButtonPrimary
+          type={'primary'}
+        >
+          Primary Button
+        </ButtonPrimary>
+
+        <ButtonPrimary
+          className={'green'}
+          type={'primary'}
+        >
+          Primary Button Green
+        </ButtonPrimary>
       </section>
     </Layout>
   )
